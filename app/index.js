77721f6588c16ec893e75c91cc4829be3d364485
Muhammed athimanil <hello@athimannil.com>
2017-08-18 21:54:26 +0200

    initial structure setup

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Github trend feeds</h3>
+        <p>helo mate</p>
+        <h1>lorem</h1>
+      </div>
+    );
+  }
+}
+
+render(<App />, window.document.getElementById('myapp'));
---
+++ webpack.config.js
@@ -0,0 +1,103 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const bootstrapEntryPoints = require('./webpack.bootstrap.config');
+
+const extractPlugin = new ExtractTextPlugin({
+  filename: '[name].css'
+});
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const SRC_DIR = path.resolve(__dirname, 'app');
+
+const config = {
+  entry: [SRC_DIR + "/index.js",bootstrapEntryPoints.dev],
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+    // publicPath: '/app/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query:{
+          presets: ['react', 'es2015', 'stage-2']
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: (loader) => [
+                  require('autoprefixer')({
+                    browsers: ['last 2 version']
+                  }),
+                  require('postcss-flexbugs-fixes')(),
+                  require('css-mqpacker')
+                ]
+              }
+            },
+            {
+              loader: 'sass-loader',
+            }
+          ]
+        })
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(jpeg|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              // outputPath: 'img/',
+              publicPath: 'img/'
+            }
+          }
+        ]
+      },
+      // { test: /\.(woff2?|svg)$/, loader: 'url-loader?limit=10000' },
+      { test: /\.(woff2?|svg)$/, use: 'url-loader?limit=10000&name=fonts/[name].[ext]' },
+      // { test: /\.(ttf|eot)$/, loader: 'file-loader' },
+      { test: /\.(ttf|eot)$/, use: 'file-loader?name=fonts/[name].[ext]' },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            }
+          }
+        ],
+        exclude: path.resolve(__dirname, 'app/index.html')
+      }
+    ]
+  },
+  plugins:[
+    extractPlugin,
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'app/index.html'
+    }),
+    new CleanWebpackPlugin(['dist'])
+  ]
+};
+
+module.exports = config;
