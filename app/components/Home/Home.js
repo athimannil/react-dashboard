61c451f1cc3da9ad5d5ed03cba4645154b22fbf5
Muhammed athimanil <hello@athimannil.com>
2017-08-26 17:08:20 +0200

    set up localStorage

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 import { Todo } from './Todo/Todo';
 
-const todolist = ['first task', 'second task', 'Final tassk'];
+var todolist = ['first task', 'second task', 'Final tassk'];
+var storedTodos = localStorage.getItem('storedTodos');
+
+if (storedTodos) {
+  todolist = JSON.parse(storedTodos);
+} else {
+  localStorage.setItem('storedTodos', JSON.stringify(todolist));
+}
 
 export class Home extends React.Component {
   render () {
